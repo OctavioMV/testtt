7687ec3902b43edc2a131726065cda9cccafa272
OctavioMV <octavio.mesa-varona@bfr.bund.de>
2023-09-28 08:41:08 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,8 +3,9 @@ import streamlit as st
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-# Create a Streamlit app title
+# Create a Streamlit app title and description
 st.title("Image Captioning with Transformers")
+st.write("Upload an image, and enter a caption to generate image captions.")
 
 # Load pre-trained models
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
@@ -14,23 +15,31 @@ model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-capt
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
-    # Load and preprocess the uploaded image
-    raw_image = Image.open(uploaded_image).convert('RGB')
+    try:
+        # Load and preprocess the uploaded image
+        raw_image = Image.open(uploaded_image).convert('RGB')
 
-    # Add a text input widget for the image caption
-    text = st.text_input("Enter a caption for the image", "a culinary dish from Montenegro consisting of")
+        # Display the uploaded image
+        st.image(raw_image, caption="Uploaded Image", use_column_width=True)
 
-    # Conditional image captioning
-    inputs = processor(raw_image, text, return_tensors="pt")
+        # Add a text input widget for the image caption
+        default_caption = "Enter a caption for the image"
+        text = st.text_input("Image Caption", default_caption)
 
-    out = model.generate(**inputs)
-    caption_conditional = processor.decode(out[0], skip_special_tokens=True)
-    st.write("Conditional Caption:", caption_conditional)
+        # Conditional image captioning
+        if text != default_caption:
+            inputs = processor(raw_image, text, return_tensors="pt")
+            out = model.generate(**inputs)
+            caption_conditional = processor.decode(out[0], skip_special_tokens=True)
+            st.write("Conditional Caption:", caption_conditional)
 
-    # Unconditional image captioning
-    inputs = processor(raw_image, return_tensors="pt")
+        # Unconditional image captioning
+        st.subheader("Unconditional Caption:")
+        inputs = processor(raw_image, return_tensors="pt")
+        out = model.generate(**inputs)
+        caption_unconditional = processor.decode(out[0], skip_special_tokens=True)
+        st.write(caption_unconditional)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
-    out = model.generate(**inputs)
-    caption_unconditional = processor.decode(out[0], skip_special_tokens=True)
-    st.write("Unconditional Caption:", caption_unconditional)
 
